c7c2d164f53f2e7bb8ada911d1aa0cbddbfc35d7
BoJackHorseman <1142083668@qq.com>
2017-09-12 17:37:09 +0800

    完善todo list

--- src/js/App.js
+++ src/js/App.js
@@ -4,7 +4,8 @@ class App extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            buyItems: ["milk", "bread", "fruit"]
+            buyItems: ["milk", "bread", "fruit"],
+            message: ""
         }
     }
 
@@ -12,16 +13,46 @@ class App extends React.Component{
         e.preventDefault();
         const {buyItems} = this.state;
         const newItem = this.newItem.value;
-
-        this.setState({
-            buyItems: [...this.state.buyItems, newItem]
-        })
+        const isOnTheList = buyItems.includes(newItem);
+        if(isOnTheList){
+            this.setState({
+                message: "The item is already on the list."
+            })
+        }else{
+            newItem !== "" && this.setState({
+                buyItems: [...this.state.buyItems, newItem],
+                message: ""
+            })
+        }
 
         this.addForm.reset()
     }
 
+    removeItem(item){
+        const newBuyItems = this.state.buyItems.filter(buyItem => {
+            return buyItem !== item;
+        });
+
+        this.setState({
+            buyItems: [...newBuyItems]
+        })
+
+        if(newBuyItems.length ===0){
+            this.setState({
+                message: "No items on your list, add some."
+            })
+        }
+    }
+
+    clearAll(){
+        this.setState({
+            buyItems: [],
+            message: "No items on your list, add some."
+        })
+    }
+
     render(){
-        const {buyItems} = this.state;
+        const {buyItems, message} = this.state;
         return(
             <div className="col-md-9">
                 <header className="panel text-center">
@@ -35,31 +66,44 @@ class App extends React.Component{
                         </div>
                     </form>
                 </header>
-                <div className="panel panel-default">
-                    <div className="panel-heading">Shopping List</div>
-                    <table className="table"> 
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Item</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                buyItems.map((item,index) => {
-                                    return (
-                                        <tr key={item}>
-                                            <th scope="row">{index+1}</th>
-                                            <td>{item}</td>
-                                            <td><button className="btn btn-default">Delete</button></td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
-                </div>
+                {
+                    (message !== "" || buyItems.length === 0) && <p className="alert alert-danger">{message}</p>
+                }
+                
+                {
+                    buyItems.length > 0 && 
+                    <div className="panel panel-default">
+                        <div className="panel-heading">Shopping List</div>
+                        <table className="table"> 
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Item</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    buyItems.map((item,index) => {
+                                        return (
+                                            <tr key={item}>
+                                                <th scope="row">{index+1}</th>
+                                                <td>{item}</td>
+                                                <td><button onClick={e => this.removeItem(item)} type="button" className="btn btn-default">Delete</button></td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td colSpan="2">&nbsp;</td>
+                                    <td><button type="button" className="btn btn-info" onClick={e => this.clearAll()}>Clear List</button></td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                }
             </div>
         )
     }
